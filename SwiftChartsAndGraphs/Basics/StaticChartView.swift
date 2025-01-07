af9cd2271f895d0a38e0e356904aa1a9df3dcb02
A.F. Adib <a.f.adib36@gmail.com>
2025-01-07 21:05:55 +0600

    static chart view

--- SwiftChartsAndGraphs/Basics/StaticChartView.swift
+++ SwiftChartsAndGraphs/Basics/StaticChartView.swift
@@ -13,13 +13,33 @@ struct StaticChartView: View {
     @State private var avgShown  = false
     
     var body: some View {
+        
         VStack {
             Chart {
                 BarMark(x: .value("Type", "Bird"),
                         y: .value("Population", 1 ))
                 .opacity(0.5)
+                
+                BarMark(x: .value("Type", "Dog"),
+                        y: .value("Population", 2 ))
+                .opacity(0.5)
+                
+                BarMark(x: .value("Type", "Cat"),
+                        y: .value("Population", 3 ))
+                
+                if avgShown {
+                    RuleMark(y: .value("Average", 1.5))
+                        .foregroundStyle(.gray)
+                        .annotation(position: .bottom, alignment: .bottomLeading) {
+                            Text("average 1.5")
+                        }
+                }
             }
+            .aspectRatio(1, contentMode: .fit)
+            
+            Toggle(avgShown ? "Show average" : "Hide average", isOn: $avgShown)
         }
+        .padding()
     }
 }
 
