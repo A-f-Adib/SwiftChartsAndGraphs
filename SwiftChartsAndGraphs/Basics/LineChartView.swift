885f3b4dbcf21f159077f6ef9929c0db43009619
A.F. Adib <a.f.adib36@gmail.com>
2025-01-07 22:48:31 +0600

    Line chart view

--- SwiftChartsAndGraphs/Basics/LineChartView.swift
+++ SwiftChartsAndGraphs/Basics/LineChartView.swift
@@ -6,10 +6,33 @@
 //
 
 import SwiftUI
+import Charts
 
 struct LineChartView: View {
+    
+    let catData = PetData.catExmpl
+    let dogData = PetData.dogExmpl
+    
+    var data: [(type: String, petData: [PetData])] {
+        [
+            (type: "cat", petData: catData),
+            (type: "dog", petData: dogData)
+        ]
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Chart(data, id: \.type) { dataSeries in
+            ForEach(dataSeries.petData) { data in
+                LineMark(x: .value("Year", data.year),
+                         y: .value("Population", data.population))
+            }
+            .foregroundStyle(by: .value("Pet type", dataSeries.type))
+            .symbol(by: .value("Pet type", dataSeries.type))
+        }
+        .chartXScale(domain: 1998...2025)
+        .aspectRatio(1, contentMode: .fit)
+        .padding()
     }
 }
 
