6afc4daa7d70cfa974a770340b0abd5e6eedecc7
A.F. Adib <a.f.adib36@gmail.com>
2025-01-09 02:21:50 +0600

    func added

--- SwiftChartsAndGraphs/Model/Sale.swift
+++ SwiftChartsAndGraphs/Model/Sale.swift
@@ -31,4 +31,28 @@ struct Sale: Identifiable, Equatable {
            Sale(id: UUID(), book: Book.examples[8], quantity: 3, saleDate: Date(timeIntervalSinceNow: -64_800_000)),
            Sale(id: UUID(), book: Book.examples[9], quantity: 4, saleDate: Date(timeIntervalSinceNow: -72_000_000))
        ]
+    
+    
+    static func threeMonthsExmpl() -> [Sale] {
+        
+        let threeMonthsAgo = Calendar.current.date(byAdding: .month, value: -3, to: Date())!
+        
+        let exampleSales: [Sale] = (1...300).map { _ in
+            let randomBook = Book.examples.randomElement()!
+            let randomQuantity = Int.random(in: 1...5)
+            let randomDate = Date.random(in: threeMonthsAgo...Date())
+            
+            return Sale(id: UUID(), book: randomBook, quantity: randomQuantity, saleDate: randomDate)
+        }
+        return exampleSales.sorted { $0.saleDate < $1.saleDate }
+    }
+}
+
+
+extension Date {
+    static func random(in range: ClosedRange<Date>) -> Date {
+        let diff = range.upperBound.timeIntervalSinceReferenceDate - range.lowerBound.timeIntervalSinceReferenceDate
+        let randomValue = diff * Double.random(in: 0...1) + range.lowerBound.timeIntervalSinceReferenceDate
+        return Date(timeIntervalSinceReferenceDate: randomValue)
+    }
 }
