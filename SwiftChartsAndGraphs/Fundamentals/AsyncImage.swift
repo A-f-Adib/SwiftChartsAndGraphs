a95e69069acb82600a4acc4f2689c10996c3cf25
A.F. Adib <a.f.adib36@gmail.com>
2025-01-15 23:34:40 +0600

    async image

--- SwiftChartsAndGraphs/Fundamentals/AsyncImage.swift
+++ SwiftChartsAndGraphs/Fundamentals/AsyncImage.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct AsyncImage: View {
+struct asyncImage: View {
     
     let url = URL(string: "https://picsum.photos/400")
     
@@ -24,6 +24,39 @@ struct AsyncImage: View {
     }
 }
 
+//same thing can be implemented by this way:
+struct AsyncImageTwo: View {
+    
+    let url = URL(string: "https://picsum.photos/400")
+    
+    var body: some View {
+        
+        AsyncImage(url: url) { phase in
+           
+            switch phase {
+                
+            case .empty:
+                ProgressView()
+                
+            case .success(let returnImg):
+                returnImg
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+                
+            case .failure:
+                Image(systemName: "questionmark")
+                    .font(.headline)
+                
+            default:
+                Image(systemName: "questionmark")
+                    .font(.headline)
+            }
+        }
+    }
+}
+
 #Preview {
-    AsyncImage()
+    asyncImage()
 }
