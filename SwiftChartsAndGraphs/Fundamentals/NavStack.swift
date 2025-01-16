8294001da328d516cfd012a7a8ba6d014ded9525
A.F. Adib <a.f.adib36@gmail.com>
2025-01-16 23:04:59 +0600

    NavStack

---
+++ SwiftChartsAndGraphs/Fundamentals/NavStack.swift
@@ -0,0 +1,20 @@
+//
+//  NavStack.swift
+//  SwiftChartsAndGraphs
+//
+//  Created by A.F. Adib on 16/1/25.
+//
+
+import SwiftUI
+
+struct NavStack: View {
+    
+    var body: some View {
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavStack()
+}
