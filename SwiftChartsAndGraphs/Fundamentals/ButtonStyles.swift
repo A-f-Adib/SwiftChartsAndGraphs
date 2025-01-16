f471fed783490ab32bf416186d3c4dc673a89e4f
A.F. Adib <a.f.adib36@gmail.com>
2025-01-16 22:55:15 +0600

    Button styles

---
+++ SwiftChartsAndGraphs/Fundamentals/ButtonStyles.swift
@@ -0,0 +1,27 @@
+//
+//  ButtonStyles.swift
+//  SwiftChartsAndGraphs
+//
+//  Created by A.F. Adib on 16/1/25.
+//
+
+import SwiftUI
+
+struct ButtonStyles: View {
+    
+    var body: some View {
+        
+        VStack{
+            
+            Button("Button styles") { }
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .buttonStyle(.plain)
+                .controlSize(.small)
+        }
+    }
+}
+
+#Preview {
+    ButtonStyles()
+}
